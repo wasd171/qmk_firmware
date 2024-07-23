5cdf5fcd54a951fdca32ceaebb2e39b5057c1a25
Konstantin Nesterov <konstantin@careerlunch.com>
2024-07-23 12:10:01 +0200

    Implement "normal" base layer

--- keyboards/wilba_tech/rama_works_m60_a/keymaps/wasd171/keymap.c
+++ keyboards/wilba_tech/rama_works_m60_a/keymaps/wasd171/keymap.c
@@ -1,9 +1,10 @@
 // M60-A layout
 #include QMK_KEYBOARD_H
 
-#define MAIN 0
-#define UTIL 1
-#define FUNC 2
+#define MAIN 		0
+#define MAIN_NORMAL 1
+#define UTIL 		2
+#define FUNC 		3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -13,7 +14,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
 	KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,      KC_SCLN, KC_QUOT, KC_ENT,
 	KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH, KC_RSPC, MO(FUNC),
-	         KC_LGUI, KC_LALT,                            LT(UTIL, KC_SPC),                     KC_RALT, _______),
+	         KC_LGUI, KC_LALT,                            LT(UTIL, KC_SPC),                     KC_RALT, DF(MAIN_NORMAL)),
+
+// Main (normal) layer (normal Space key)
+[MAIN_NORMAL] = LAYOUT_60_hhkb(
+	KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,      KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
+	KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,      KC_SCLN, KC_QUOT, KC_ENT,
+	KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH, KC_RSPC, MO(FUNC),
+	         KC_LGUI, KC_LALT,                            KC_SPC,                     			KC_RALT, DF(MAIN)),
 
 // Util layer
 [UTIL] = LAYOUT_60_hhkb(
